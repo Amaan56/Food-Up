58559d651b361cb249a2a618690d2ba0a911fdcf
Amaan56 <shaikhamaan257@gmail.com>
2021-11-19 09:48:53 +0530

    feat: add addToCart handler to context

--- src/store/order-context.js
+++ src/store/order-context.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import initialState from './initial-state';
 
-const OrderContext = React.createContext(initialState);
+const OrderContext = React.createContext({
+  state: initialState,
+  addToCart: (item) => {},
+});
 
 export default OrderContext;
